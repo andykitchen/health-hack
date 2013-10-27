3daaa327ad58e447b05282e1e70f79aefd9b9a10
Tobias Sargeant <sargeant@wehi.edu.au>
2013-10-27 11:26:12 +1100

    added /data/ordered that returns genes in order of increasing mean coverage.

--- haloplex/haloplex.py
+++ haloplex/haloplex.py
@@ -2,19 +2,42 @@ from flask import *
 
 import sys
 import os
+import pandas
 
 data = json.load(open('data/normalized_data.json', 'r'))
+
+metadata = pandas.DataFrame(data, columns = [ 'chr', 'start', 'end', 'gene' ])
+samples = pandas.DataFrame(data['samples'], columns = sorted(data['samples'].keys()))
+
+
 
 class MyFlask(Flask):
   def get_send_file_max_age(self, name):
     return 0
 
+
+
 app = MyFlask(__name__)
 app.config.from_object(__name__)
+
+
+def build_data(metadata_rows, sample_rows):
+  return dict(
+    chr = list(metadata_rows.chr),
+    start = list(metadata_rows.start),
+    end = list(metadata_rows.end),
+    gene = list(metadata_rows.end),
+    samples = { k:list(sample_rows[k]) for k in sorted(sample_rows.columns) }
+  )
 
 @app.route('/')
 def index_html():
   return render_template('index.html')
+
+@app.route('/data/ordered')
+def ordered():
+  sort_order = samples.mean(axis=1).argsort()
+  return jsonify(build_data(metadata.ix[sort_order], samples.ix[sort_order]))
 
 @app.route('/normalized_data.json')
 def return_data():
