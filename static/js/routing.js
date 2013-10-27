26cc121b9d6a2ed571a1e94e0d47f76c3c9783ae
Jonathan Chang <jonathan.chang@silverpond.com.au>
2013-10-27 15:24:06 +1100

    handle single vs multiple sample id selections

--- static/js/routing.js
+++ static/js/routing.js
@@ -1,18 +1,23 @@
 function transform_url() {
   params = $.url().param()
-  if (params["i"]) {
+  if (typeof params["i"] === 'array') {
     params["i"] = params["i"].join(",")
-  }
+  } 
   sanitized =  _.omit(params, function(v) { return v == ""});
-  
-  return "/data/ordered?" + $.param(sanitized);
+  url_params = ""
+  _.forIn( sanitized, function(v, k) {
+    if (url_params.length > 0) {
+      url_params = url_params + "&";
+    }
+    url_params = url_params + k + "=" + v;
+  });
+  return "/data/ordered?" + url_params; //$.param(sanitized);
 }
 
 function set_default_form_values() {
   if ($.url().param("x") == "zscore") {
     $('#z-score').prop("checked", "checked");
   }
-
   $('#row-limit').val( $.url().param("c") );
   $('#row-offset').val( $.url().param("s") );
   $('#order').val( $.url().param("o") );
